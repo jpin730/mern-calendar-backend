4885c30e7440f3f2feb14c89bebe627aeff63720
Jaime Pineda <jpin730@pm.me>
2023-06-15 13:13:32 -0600

    chore: :package: add express and dotenv

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(req.method);
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`);
+});
