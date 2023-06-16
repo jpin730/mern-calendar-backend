8588b61fe9e90e27ad7ae3d060064ab7e55e68a5
Jaime Pineda <jpin730@pm.me>
2023-06-16 13:02:39 -0600

    feat: :sparkles: add login with database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
+const bcrypt = require("bcryptjs");
+
 const User = require("../models/User");
-const encrypt = require("../utils/encrypt");
 
 const createUser = async (req, res) => {
   try {
@@ -15,7 +16,7 @@ const createUser = async (req, res) => {
 
     const user = new User(req.body);
 
-    user.password = encrypt(password);
+    user.password = bcrypt.hashSync(password, bcrypt.genSaltSync());
 
     await user.save();
 
@@ -23,15 +24,37 @@ const createUser = async (req, res) => {
       .status(201)
       .json({ ok: true, msg: "User created", uid: user.id, name: user.name });
   } catch (error) {
-    console.log(error.message);
-    res.status(500).json({ ok: false, msg: "User cannot be created" });
+    res.status(500).json({ ok: false, msg: "Server error" });
   }
 };
 
-const loginUser = (req, res) => {
-  const { email, password } = req.body;
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  res.status(201).json({ ok: true, msg: "Logged in user" });
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ ok: false, msg: "User email does not exist" });
+    }
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword) {
+      return res
+        .status(400)
+        .json({ ok: false, msg: "User password is incorrect" });
+    }
+    // await user.save();
+
+    res
+      .status(201)
+      .json({ ok: true, msg: "Logged in user", uid: user.id, name: user.name });
+  } catch (error) {
+    res.status(500).json({ ok: false, msg: "Server error" });
+  }
 };
 
 const refreshToken = (req, res) => {
--- utils/encrypt.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-const encrypt = (value) => bcrypt.hashSync(value, bcrypt.genSaltSync());
-
-module.exports = encrypt;
