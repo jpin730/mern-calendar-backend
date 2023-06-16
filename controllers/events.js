b31c5b205d71d44cc643804839f976f8fa5ad0ad
Jaime Pineda <jpin730@pm.me>
2023-06-16 17:53:02 -0600

    feat: :sparkles: delete event with database

--- controllers/events.js
+++ controllers/events.js
@@ -56,8 +56,29 @@ const updateEvent = async (req, res) => {
   }
 };
 
-const deleteEvent = (req, res) => {
-  res.json({ ok: true, msg: "deleteEvent" });
+const deleteEvent = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const event = await Event.findById(id);
+
+    if (!event) {
+      return res.status(404).json({ ok: false, msg: "Event does not exist" });
+    }
+
+    if (event.user.toString() !== req.uid) {
+      return res.status(403).json({
+        ok: false,
+        msg: "Event does not belong to the user",
+      });
+    }
+
+    const deletedEvent = await Event.findByIdAndDelete(id);
+
+    res.json({ ok: true, msg: "Event deleted", deletedEvent });
+  } catch {
+    res.status(500).json({ ok: false, msg: "Server error" });
+  }
 };
 
 module.exports = { getAllEvents, createEvent, updateEvent, deleteEvent };
