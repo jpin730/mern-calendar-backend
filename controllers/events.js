ef42df4c76fe63d90e2f94c23790b54b1c47a833
Jaime Pineda <jpin730@pm.me>
2023-06-16 17:25:00 -0600

    feat: :sparkles: list all events

--- controllers/events.js
+++ controllers/events.js
@@ -1,7 +1,13 @@
 const Event = require("../models/Events");
 
-const getAllEvents = (req, res) => {
-  res.json({ ok: true, msg: "getAllEvents" });
+const getAllEvents = async (req, res) => {
+  try {
+    const events = await Event.find().populate("user", "name");
+
+    res.json({ ok: true, events });
+  } catch {
+    res.status(500).json({ ok: false, msg: "Server error" });
+  }
 };
 
 const createEvent = async (req, res) => {
@@ -12,7 +18,7 @@ const createEvent = async (req, res) => {
 
     const createdEvent = await event.save();
 
-    res.status(201).json({ ok: true, msg: "createEvent", createdEvent });
+    res.status(201).json({ ok: true, msg: "Event created", createdEvent });
   } catch {
     res.status(500).json({ ok: false, msg: "Server error" });
   }
