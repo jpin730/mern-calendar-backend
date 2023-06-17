7eb49152f5ff41b0603554b560da28ab298ed036
Jaime Pineda <jpin730@pm.me>
2023-06-17 17:03:13 -0600

    chore: :green_heart: fix deploy to railway

--- database/config.js
+++ database/config.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const dbConnection = async () => {
-  try {
-    await mongoose.connect(process.env.DB_CNN, {});
-    console.log("Database connected.");
-  } catch {
-    throw new Error("Error on database connection.");
-  }
+  // try {
+  await mongoose.connect(process.env.DB_CNN, {});
+  console.log("Database connected.");
+  // } catch {
+  //   throw new Error("Error on database connection.");
+  // }
 };
 
 module.exports = { dbConnection };
