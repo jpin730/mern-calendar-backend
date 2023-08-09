47e3e06f59c9c950d1d73adf8261199c94a0e31d
Jaime Pineda <jpin730@pm.me>
2023-08-08 19:07:23 -0600

    build: :rocket: deploy to vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
-const { dbConnection } = require("./database/config");
+const { dbConnection } = require("../database/config");
 
 const app = express();
 
@@ -11,12 +11,12 @@ dbConnection();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/events", require("./routes/events"));
+app.use("/api/auth", require("../routes/auth"));
+app.use("/api/events", require("../routes/events"));
 
-app.get("*", (req, res) => {
+app.get("*", (_, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
